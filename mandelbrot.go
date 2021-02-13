98bce81b3bde1f16f813a8161c5a1965e6ed87d8
Thomas McRoberts <wmcroberts02@qub.ac.uk>
2021-02-13 17:55:53 +0000

    Change palette to lookup
    Changed palette to a lookup table to boost performance and add some time
    logging.

--- mandelbrot.go
+++ mandelbrot.go
@@ -6,10 +6,11 @@ import (
 	"image/color"
 	"image/png"
 	"os"
+	"time"
 )
 
 const (
-	maxIteration = 1000
+	maxIteration = 10000
 	width        = 3840
 	height       = 2160
 )
@@ -18,9 +19,11 @@ func main() {
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{width, height}
+	palette := calculatePalette()
 
+	fmt.Println("Creating image...")
+	startTime := time.Now()
 	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
-	// Set color for each pixel.
 	for Px := 0; Px < width; Px++ {
 		for Py := 0; Py < height; Py++ {
 			x0, y0 := mandelbrotScale(Px, Py)
@@ -32,15 +35,15 @@ func main() {
 				x = xtemp
 				iteration++
 			}
-			color := palette(iteration)
+			color := palette[iteration-1]
 			img.Set(Px, Py, color)
 		}
 	}
 
 	// Encode as PNG.
-	fmt.Println("Creating image...")
 	f, _ := os.Create("images/image.png")
 	png.Encode(f, img)
+	fmt.Printf("Done, took %s\n", time.Since(startTime))
 }
 
 func mapVal(x, imin, imax, omin, omax float64) float64 {
@@ -53,7 +56,11 @@ func mandelbrotScale(x, y int) (float64, float64) {
 	return scaledx, scaledy
 }
 
-func palette(x uint32) color.RGBA {
-	newx := uint32(mapVal(float64(x), 0.0, float64(maxIteration), float64(0xFFFFFF), 0.0))
-	return color.RGBA{uint8(newx >> 16), uint8(newx >> 8), uint8(newx), 0xFF}
+func calculatePalette() []color.RGBA {
+	pal := make([]color.RGBA, maxIteration, maxIteration)
+	for i := 0; i < maxIteration; i++ {
+		inew := uint32(mapVal(float64(i), 0.0, float64(maxIteration-1), float64(0xFFFFFF), 0.0))
+		pal[i] = color.RGBA{uint8(inew >> 16), uint8(inew >> 8), uint8(inew), 0xFF}
+	}
+	return pal
 }
